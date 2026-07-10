f876c9ccfa4c3b6f1f18373cd05aa33f11dd927e
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2026-07-10 18:27:16 +0100

    new features

--- master/settings.py
+++ master/settings.py
@@ -10,7 +10,8 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     'watch2d.vercel.app',
